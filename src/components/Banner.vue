7e8cb14f40baa3a1412f92e6b8970b150415934b
Lin <linyulin130706012@163.com>
2019-04-20 23:15:33 +0800

    完成banner轮播图

---
+++ src/components/Banner.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="swiper-container billboards">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(banner ,index) in banners" :key="index">
+        <img :src="banner.imgUrl" alt>
+      </div>
+      <!-- <div class="swiper-slide">
+        <img src="https://static.maizuo.com/v5/upload/831f44034b9416bf2dd579f0921f676e.jpg" alt>
+      </div>
+      <div class="swiper-slide">
+        <img src="https://static.maizuo.com/v5/upload/6a0c5b59de9107732da8edd7a0ed7142.jpg" alt>
+      </div>-->
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+import "swiper/dist/css/swiper.css";
+export default {
+  props: {
+    banners: {
+      type: Array
+    }
+  },
+
+  data() {
+    return {
+      swiper: null
+    };
+  },
+
+  updated() {
+    this.swiper = new Swiper(".swiper-container", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    });
+  }
+};
+</script>
+
+<style lang="less">
+.swiper-container {
+  width: 100%;
+  height: 210px;
+  img {
+    width: 100%;
+  }
+  .swiper-pagination-bullet {
+    background-color: #fff;
+    opacity: 1;
+  }
+  .swiper-pagination-bullet-active {
+    background-color: #f00;
+  }
+}
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,5 +1,49 @@
 <template>
   <div>
-    电影首页
+    <Banner :banners="bannerList"></Banner>
   </div>
 </template>
+
+<script>
+import Banner from "../components/Banner.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    Banner: Banner
+  },
+
+  data() {
+    return {
+      bannerList: []
+    };
+  },
+
+  methods: {
+    //请求轮播图数据
+    getBannerList() {
+      axios
+        .get("https://m.maizuo.com/gateway?type=2&cityId=440300&k=7347501", {
+          headers: {
+            "X-Client-Info":
+              '{"a":"3000","ch":"1002","v":"5.0.4","e":"15546493441941325218476"}',
+            "X-Host": "mall.cfg.common-banner"
+          }
+        })
+        .then(res => {
+          // console.log(res.data);
+          let data = res.data;
+          if (data.status == 0) {
+            this.bannerList = data.data;
+          } else {
+            // console.log(data.msg);
+          }
+        });
+    }
+  },
+
+  created() {
+    this.getBannerList();
+  }
+};
+</script>
